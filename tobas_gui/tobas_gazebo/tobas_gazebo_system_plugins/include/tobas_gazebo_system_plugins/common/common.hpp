14d66a17fde949a1f37cc82d0d391eb13927772c
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-19 12:33:09 +0900

    Trimmed the branch to only the tobas_gui/tobas_gazebo/tobas_gazebo_tools sub directory

--- tobas_gui/tobas_gazebo/tobas_gazebo_system_plugins/include/tobas_gazebo_system_plugins/common/common.hpp
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2026 Tobas, Inc.
-
-#pragma once
-
-#include "./constants.hpp"
-#include "./definitions.hpp"
-#include "./node.hpp"
-#include "./plugin.hpp"
