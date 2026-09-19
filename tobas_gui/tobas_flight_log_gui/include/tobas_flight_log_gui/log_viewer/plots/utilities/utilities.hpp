1b4b7d89385bd69c336a30ee212838a54f210ea5
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-19 12:46:33 +0900

    Trimmed the branch to only the tobas_core/tobas_description sub directory

--- tobas_gui/tobas_flight_log_gui/include/tobas_flight_log_gui/log_viewer/plots/utilities/utilities.hpp
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2026 Tobas, Inc.
-
-#pragma once
-
-#include "./base_plot.hpp"
-#include "./constants.hpp"
-#include "./qwt_plot.hpp"
-#include "./vertical_scale.hpp"
