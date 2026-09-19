e8b9d9c9c2c728366b08b682625f4e1754c8e9b1
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-19 12:37:03 +0900

    Trimmed the branch to only the tobas_core/tobas_property/tobas_property sub directory

--- tobas_core/tobas_drone/tobas_drone_core/include/tobas_drone_core/propulsion_system/ice_propulsion_system/aerodynamics.hpp
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2026 Tobas, Inc.
-
-#pragma once
-
-#include "./drag_constant.hpp"
-#include "./moment_constant.hpp"
-#include "./motor_constant.hpp"
