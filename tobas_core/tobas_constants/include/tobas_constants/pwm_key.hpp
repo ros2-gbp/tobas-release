7076bd4ae2951fd135268c3c46fc0ec58dd88ede
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-22 14:58:53 +0900

    Revert "Trimmed the branch to only the tobas_gui/tobas_ssh/tobas_ssh_msgs sub directory"
    This reverts commit 5cfa18515c8bd6ef4cceaca45dc4dfee020349f8.

---
+++ tobas_core/tobas_constants/include/tobas_constants/pwm_key.hpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2026 Tobas, Inc.
+
+#pragma once
+
+namespace tobas
+{
+namespace pwm_key
+{
+static constexpr char kEngineThrottleKey[] = "engine";
+}  // namespace pwm_key
+}  // namespace tobas
