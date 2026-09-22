d76172d3ac1b8bc633bfdb1682bdfb187e5ce58f
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-22 15:40:43 +0900

    Rebase from 'release/jazzy/tobas_tile_proxy'

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
-import os
 import os.path as osp
 from glob import glob
-from typing import List
 from setuptools import setup, find_packages
 
-pkg_name = osp.basename(osp.abspath(osp.curdir))
-
-
-def package_files(directory: str, data_files: List[str]) -> List[str]:
-    for path, _, filenames in os.walk(directory):
-        for _ in filenames:
-            data_files.append((f"share/{pkg_name}/{path}", glob(path + "/**/*.*", recursive=True)))
-    return data_files
-
+pkg_name = "tobas_tile_proxy"
 
 data_files = []
 data_files.append(("share/ament_index/resource_index/packages", ["resource/" + pkg_name]))
@@ -25,15 +15,9 @@ for node_file in glob(f"{pkg_name}/*_node.py"):
 
 setup(
     name=pkg_name,
-    version="2.16.1",
+    version="2.16.2",
     packages=find_packages(),
     data_files=data_files,
     install_requires=["setuptools"],
-    zip_safe=True,
-    maintainer="Masayoshi Dohi",
-    maintainer_email="m.dohi@tobas.jp",
-    description="TODO",
-    license="TODO",
-    tests_require=["pytest"],
     entry_points={"console_scripts": console_scripts},
 )
