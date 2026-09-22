97cd6867790d09a915b34a1f081b42f557308676
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-22 15:06:20 +0900

    Revert "Trimmed the branch to only the tobas_core/tobas_common/tobas_kdl_msgs sub directory"
    This reverts commit b00f232744b46e8b3f3430ccbfe09310d3ce9c09.

---
+++ tobas_core/tobas_constants/include/tobas_constants/node.hpp
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2026 Tobas, Inc.
+
+#pragma once
+
+namespace tobas
+{
+// Node names
+namespace node
+{
+static constexpr char kImuFilterConfigServer[] = "imu_filter_config_server";
+static constexpr char kRpmControlConfigServer[] = "rpm_control_config_server";
+static constexpr char kObserver[] = "observer";
+static constexpr char kController[] = "controller";
+static constexpr char kRcTeleop[] = "rc_teleop";
+static constexpr char kMissionExecutor[] = "mission_executor";
+}  // namespace node
+}  // namespace tobas
