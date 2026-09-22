6dd093deae71a95c83683404050ae8170ca1c04f
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-22 15:56:11 +0900

    Trimmed the branch to only the tobas_core/tobas_property/tobas_property sub directory

--- tobas_gui/tobas_control_system/include/tobas_control_system/mission_planner/commands/commands.hpp
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2026 Tobas, Inc.
-
-#pragma once
-
-#include "./land.hpp"
-#include "./return_to_launch.hpp"
-#include "./takeoff.hpp"
-#include "./waypoint.hpp"
