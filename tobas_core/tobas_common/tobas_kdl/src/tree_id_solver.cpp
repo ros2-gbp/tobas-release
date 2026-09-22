7e9f99a6c1d47f4b1276b6471d894d41ce4dcb8c
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-22 15:52:22 +0900

    Revert "Trimmed the branch to only the tobas_gui/tobas_bootmedia_config sub directory"
    This reverts commit e81aa35b69837a6707d300983301c93986340beb.

---
+++ tobas_core/tobas_common/tobas_kdl/src/tree_id_solver.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Tobas, Inc.
+
+#include "tobas_kdl/tree_id_solver.hpp"
+
+namespace tobas
+{
+namespace kdl
+{
+TreeIdSolver::TreeIdSolver(const Tree& tree) : super(tree)
+{
+}
+}  // namespace kdl
+}  // namespace tobas
