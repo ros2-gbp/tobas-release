8f37865a7b7acf8939e1654dd3af44adfa32b348
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-22 16:05:06 +0900

    Revert "Trimmed the branch to only the tobas_core/tobas_cui/tobas_cui sub directory"
    This reverts commit 548447e4ceaa0a0fe846ca6569ca9d96eb19ceaa.

---
+++ tobas_gui/tobas_qt_tools/include/tobas_qt_tools/event.hpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Tobas, Inc.
+
+#pragma once
+
+namespace tobas
+{
+namespace qt
+{
+/* Process all queued events. */
+void processAllQueuedEvents();
+}  // namespace qt
+}  // namespace tobas
