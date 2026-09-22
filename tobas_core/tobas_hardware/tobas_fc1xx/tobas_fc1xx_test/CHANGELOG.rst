716e8d01fd6da2c43524e20b12daba84310530f0
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-22 16:04:05 +0900

    Revert "Trimmed the branch to only the tobas_core/tobas_real/tobas_dynamixel/tobas_dynamixel_ros_interface sub directory"
    This reverts commit e977b401ac7b80e239ab8544b9311caf1b43bb14.

---
+++ tobas_core/tobas_hardware/tobas_fc1xx/tobas_fc1xx_test/CHANGELOG.rst
@@ -0,0 +1,70 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package tobas_fc1xx_test
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+2.16.2 (2026-09-22)
+-------------------
+* revert version
+* update CHANGELOG.rst
+* modify: Bump version to 2.16.2
+* Contributors: Masayoshi Dohi
+
+2.16.1 (2026-09-18)
+-------------------
+* v2.16.1
+* Contributors: Masayoshi Dohi
+
+2.16.0 (2026-07-29)
+-------------------
+* Add description
+* 0.0.0 -> 2.16.0
+* .
+* Merge branch 'feature/gcs_layout' into jazzy
+* style: use explicit decimal literals
+* Merge branch 'feature/english_translation' into jazzy
+* wip
+* Contributors: Masayoshi Dohi
+
+2.15.0 (2026-06-29)
+-------------------
+* Merge branch 'jazzy-dev' into jazzy
+* Merge branch 'feature/fc2xx_driver' into jazzy-dev
+* .
+* move drivers: ILPS22QS, IIS2MDC
+* add license
+* add license
+* refactor: ZED-F9P driver
+* Merge branch 'feature/add_namespace' into jazzy-dev
+* add namespace
+* Merge branch 'jazzy-dev' into feature/objetc_avoidance
+* add namespace
+* Merge branch 'jazzy-dev' into jazzy
+* add compile option: -Wswitch-enum
+* Contributors: Masayoshi Dohi, Tetsuka Daiki
+
+2.12.1 (2026-02-18 12:13:14 +0900)
+----------------------------------
+
+2.12.0 (2026-02-18 12:13:14 +0900)
+----------------------------------
+
+2.11.0 (2026-02-13)
+-------------------
+
+2.10.0 (2026-01-29)
+-------------------
+* Merge branch 'jazzy-dev' into jazzy
+* .
+* サンプリング周期などを変更
+* wait before each samplings
+* .
+* Merge branch 'jazzy-dev' into jazzy
+* clang-format
+* change: T1 -> FC1xx
+* Contributors: Masayoshi Dohi
+
+2.8.0 (2025-09-25)
+------------------
+
+2.7.0 (2025-09-09)
+------------------
---
+++ tobas_core/tobas_hardware/tobas_fc2xx/tobas_fc2xx_ros/nodes/baro_driver.cpp
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Tobas, Inc.
+
+#include <tobas_constants/ros_interface.hpp>
+#include <tobas_ic_drivers/stmicro/ilps22qs.hpp>
+#include <tobas_node/node.hpp>
+
+#include <tobas_msgs/msg/fluid_pressure.hpp>
+
+#include "./common.hpp"
+
+using namespace std::chrono_literals;
+
+namespace tobas
+{
+namespace fc2xx
+{
+class BaroDriverNode : public BaseNode
+{
+  static constexpr auto kSamplingPeriod = 20ms;
+
+  using self = BaroDriverNode;
+  using super = BaseNode;
+
+public:
+  explicit BaroDriverNode(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
+
+private:
+  stm::ILPS22QS baro_;
+  ros2::PublisherPtr<tobas_msgs::msg::FluidPressure> baro_pub_;
+  ros2::TimerPtr initialize_timer_, main_timer_;
+
+  void initialize();
+  void mainTimerCb();
+};
+
+BaroDriverNode::BaroDriverNode(const rclcpp::NodeOptions& options)
+  : super("fc2xx_baro_driver", nodeOptions_Default(options))
+{
+  initialize_timer_ = createWallTimer(kRetryInitializationInterval, &self::initialize, this);
+}
+
+void BaroDriverNode::initialize()
+{
+  if (!baro_.initialize("/dev/i2c-1")) {
+    TOBAS_ERROR("Failed to initialize Barometer. Retrying...");
+    return;
+  }
+
+  baro_pub_ = createPublisher<tobas_msgs::msg::FluidPressure>(topic::kAirPressure);
+
+  initialize_timer_->cancel();
+  main_timer_ = createWallTimer(kSamplingPeriod, &self::mainTimerCb, this);
+}
+
+void BaroDriverNode::mainTimerCb()
+{
+  // Create messages.
+  auto msg = std::make_unique<tobas_msgs::msg::FluidPressure>();
+
+  // Fill headers.
+  msg->header.stamp = now();
+
+  // Read sensor.
+  if (!baro_.readPressure(msg->pressure)) {
+    TOBAS_FATAL("Failed to read barometer.");
+    return;
+  }
+
+  // Publish message.
+  baro_pub_->publish(std::move(msg));
+}
+}  // namespace fc2xx
+}  // namespace tobas
+
+RCLCPP_COMPONENTS_REGISTER_NODE(tobas::fc2xx::BaroDriverNode)
