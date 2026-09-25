f9ba85a6b47a0b287a0c6d7de212d4640b6f985a
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-25 15:23:58 +0900

    Trimmed the branch to only the tobas_core/tobas_hardware/tobas_fc1xx/tobas_fc1xx_core sub directory

--- tobas_core/tobas_rosbag_recorder/src/rosbag_recorder_node.cpp
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2026 Tobas, Inc.
-
-#include <rclcpp_components/register_node_macro.hpp>
-
-#include "./rosbag_recorder.hpp"
-
-RCLCPP_COMPONENTS_REGISTER_NODE(tobas::RosbagRecorderNode)
