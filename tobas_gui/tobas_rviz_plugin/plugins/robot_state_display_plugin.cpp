f9fb034a52ee1524f412022083f29b8620a01df9
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-25 15:19:54 +0900

    Trimmed the branch to only the tobas_examples/tobas_examples_cpp sub directory

--- tobas_gui/tobas_rviz_plugin/plugins/robot_state_display_plugin.cpp
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2026 Tobas, Inc.
-
-#include <pluginlib/class_list_macros.hpp>
-
-#include "tobas_rviz_plugin/robot_state_display.hpp"
-
-PLUGINLIB_EXPORT_CLASS(tobas::RobotStateDisplay, rviz_common::Display)
