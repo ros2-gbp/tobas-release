a556226d65b95d9c9c31f1ac094d0775f7ad030a
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-25 15:23:28 +0900

    Revert "Trimmed the branch to only the tobas_core/tobas_hardware/tobas_fc2xx/tobas_fc2xx_test sub directory"
    This reverts commit 760db652f78a812e822a6f81a2a6616e7d7222cb.

---
+++ tobas_gui/tobas_rviz_plugin/include/tobas_rviz_plugin/joint_model/fixed_joint_model.hpp
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Tobas, Inc.
+
+#pragma once
+
+#include "./joint_model.hpp"
+
+namespace tobas
+{
+class FixedJointModel : public JointModel
+{
+public:
+  explicit FixedJointModel(const std::string& name, size_t joint_index, size_t first_variable_index);
+
+  void getVariableDefaultPositions(double* values) const override;
+  void computeTransform(const double* joint_values, Eigen::Isometry3d& transf) const override;
+  void computeVariablePositions(const Eigen::Isometry3d& transf, double* joint_values) const override;
+};
+}  // namespace tobas
