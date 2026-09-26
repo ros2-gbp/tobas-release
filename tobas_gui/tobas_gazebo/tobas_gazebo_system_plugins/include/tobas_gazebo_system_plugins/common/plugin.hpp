4e28c5519fb750c387b8609fbd6387374c3eec12
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-26 09:23:35 +0900

    Trimmed the branch to only the tobas_core/tobas_drone/tobas_drone_server sub directory

--- tobas_gui/tobas_gazebo/tobas_gazebo_system_plugins/include/tobas_gazebo_system_plugins/common/plugin.hpp
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2026 Tobas, Inc.
-
-#pragma once
-
-#include <gz/plugin/Register.hh>
-#include <gz/sim/System.hh>
-#include <gz/sim/components.hh>
