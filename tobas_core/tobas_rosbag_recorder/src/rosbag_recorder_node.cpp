0079e8a93dbed6d193436a4c34f86b2e0c880c4a
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-26 09:27:19 +0900

    Revert "Trimmed the branch to only the tobas_core/tobas_controllers/tobas_planar_multi_controller sub directory"
    This reverts commit 0e08ff05fee90a36cf2328773c624d959f92dd5f.

---
+++ tobas_core/tobas_rosbag_recorder/src/rosbag_recorder_node.cpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Tobas, Inc.
+
+#include <rclcpp_components/register_node_macro.hpp>
+
+#include "./rosbag_recorder.hpp"
+
+RCLCPP_COMPONENTS_REGISTER_NODE(tobas::RosbagRecorderNode)
