25d55b256014f41cb4c19cc581c162825e837c13
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-26 09:53:55 +0900

    Rebase from 'release/jazzy/tobas_ssh_server'

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ for node_file in glob(f"{pkg_name}/*_node.py"):
 
 setup(
     name=pkg_name,
-    version="2.16.4",
+    version="2.16.5",
     packages=find_packages(),
     data_files=data_files,
     install_requires=["setuptools"],
