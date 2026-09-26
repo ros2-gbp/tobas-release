8341f1f264b2eb154e6eeca9ee500f655e28fd59
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-26 09:17:36 +0900

    Revert "Trimmed the branch to only the tobas_gui/tobas_rviz_plugin sub directory"
    This reverts commit ba5a2a5caec4beee5ab0a58ae9a29d8756e5c15e.

---
+++ tobas_gui/tobas_setup_assistant/include/tobas_setup_assistant/setting_tabs/fixed_wing/constants.hpp
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Tobas, Inc.
+
+#pragma once
+
+namespace tobas
+{
+namespace gui
+{
+namespace sa
+{
+namespace fw
+{
+static constexpr int kStabilityCoefDecimals = 6;
+}  // namespace fw
+}  // namespace sa
+}  // namespace gui
+}  // namespace tobas
