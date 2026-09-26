a9a9b7541e0a59a28b0c635a342951c7e85e9de8
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-26 09:26:32 +0900

    Revert "Trimmed the branch to only the tobas_core/tobas_common/tobas_geographic sub directory"
    This reverts commit 5606d1c97e548a7f1e2258ca9a0a423b6c00de36.

---
+++ tobas_gui/tobas_control_system/include/tobas_control_system/util.hpp
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Tobas, Inc.
+
+#pragma once
+
+#include <filesystem>
+
+namespace tobas
+{
+namespace gui
+{
+namespace ctrl
+{
+std::filesystem::path getPkgShareDir();
+}  // namespace ctrl
+}  // namespace gui
+}  // namespace tobas
