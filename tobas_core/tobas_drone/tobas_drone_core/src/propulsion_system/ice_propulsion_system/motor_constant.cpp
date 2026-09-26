a1470a21506edf021bbf44d262364a1ceb613e13
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-26 09:20:36 +0900

    Revert "Trimmed the branch to only the tobas_core/tobas_disturbance_observer sub directory"
    This reverts commit 42533eefe176ffec780db680d5a0f76bee073d2f.

---
+++ tobas_core/tobas_drone/tobas_drone_core/src/propulsion_system/ice_propulsion_system/motor_constant.cpp
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Tobas, Inc.
+
+#include "tobas_drone_core/propulsion_system/ice_propulsion_system/motor_constant.hpp"
+
+#include <iostream>
+
+#include <tobas_yaml_tools/core.hpp>
+#include <tobas_yaml_tools/format.hpp>
+
+namespace tobas
+{
+bool VppMotorConstant::isValid() const
+{
+  if (c1 <= 0.0) {
+    std::cerr << "The second term of the motor constant must be positive." << std::endl;
+    return false;
+  }
+
+  return true;
+}
+
+bool VppMotorConstant::load(const YAML::Node& node)
+{
+  if (!yaml::load(kC0Key, node, c0)) {
+    return false;
+  }
+
+  if (!yaml::load(kC1Key, node, c1)) {
+    return false;
+  }
+
+  return true;
+}
+
+YAML::Node VppMotorConstant::dump() const
+{
+  YAML::Node node(YAML::NodeType::Map);
+
+  node[kC0Key] = yaml::format(c0);
+  node[kC1Key] = yaml::format(c1);
+
+  return node;
+}
+}  // namespace tobas
