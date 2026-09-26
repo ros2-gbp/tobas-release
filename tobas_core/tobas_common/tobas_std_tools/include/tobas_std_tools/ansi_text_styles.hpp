1aa62f65c9f2622275d3a683a3c458722dad61c5
Masayoshi Dohi <m.dohi@tobas.jp>
2026-09-26 09:18:06 +0900

    Revert "Trimmed the branch to only the tobas_gui/tobas_ssh/tobas_ssh sub directory"
    This reverts commit 8777ae948dacaeed49b14f685ae4d15c1df1c8e8.

---
+++ tobas_core/tobas_common/tobas_std_tools/include/tobas_std_tools/ansi_text_styles.hpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Tobas, Inc.
+
+#pragma once
+
+#define BLACK_PREFIX "\033[30m"
+#define RED_PREFIX "\033[31m"
+#define GREEN_PREFIX "\033[32m"
+#define YELLOW_PREFIX "\033[33m"
+#define BLUE_PREFIX "\033[34m"
+#define MAGENTA_PREFIX "\033[35m"
+#define CYAN_PREFIX "\033[36m"
+#define WHITE_PREFIX "\033[37m"
+
+#define COLOR_RESET "\033[0m"
